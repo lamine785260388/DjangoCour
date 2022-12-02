38c64301fa84a8c8108b4f8943856f82179e91cf
lamine785260388 <lamine785260388@gmail.com>
2022-12-02 23:38:08 +0000

    lien

--- djangoprojet/vente/urls.py
+++ djangoprojet/vente/urls.py
@@ -2,6 +2,6 @@ from django.contrib import admin
 from django.urls import include,path
 from . import views
 urlpatterns = [
-    path('',views.index),
-    path('<int:id>',views.show)
+    path('',views.index,name='vente_index'),
+    path('<int:id>',views.show,name='show_produit')
 ]
